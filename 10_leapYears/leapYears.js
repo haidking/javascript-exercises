74e28049e311fe1fb935dac2b58584a1542e0bf9
Haider Abbas <16145613+haidking@users.noreply.github.com>
2025-09-10 17:30:43 +0500

    Finished exercise number ten

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true
+            } else {
+                return false;
+            }
+        }
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// My errors:
+// 1. Code is hard to understand 
+// Not using proper naming conventions and control flow
