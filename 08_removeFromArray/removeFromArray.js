dd5e9c74a4364acb87533a03c5b3272059231921
Haider Abbas <16145613+haidking@users.noreply.github.com>
2025-09-10 11:49:39 +0500

    Finished exercise number eight

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let newArr = arr.filter((item) => !num.includes(item))
+    return newArr;
 };
 
 // Do not edit below this line
